921f93af7cedd4e8daff360fc9fa74b485d6b03f
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-05-02 18:04:44 +0300

    hover:text-primary cursor-pointer

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -1,29 +1,27 @@
 import Logo from "../ui/Logo";
 const Header = () => {
   return (
-    <div>
-      <div className="h-[5.5rem] bg-secondary">
-       Burgerlerimiz
+    <div className="h-[5.5rem] bg-secondary">
+      <div className="container mx-auto text-white flex justify-between ">
+        <Logo />
+        <nav>
+          <ul className="flex gap-x-2">
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Home</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Menu</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">About</a>
+            </li>
+            <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
+              <a href="">Book Table</a>
+            </li>
+          </ul>
+        </nav>
+       
       </div>
-      <div>
-       <Logo />
-      </div>
-      <nav>
-     <ul>
-      <li>
-        <a href="">Home</a>
-      </li>
-      <li>
-        <a href="">Menü</a>
-      </li>
-      <li>
-        <a href="">About</a>
-      </li>
-      <li>
-        <a href="">Book Table</a>
-      </li>
-     </ul>
-      </nav>
     </div>
   )
 }
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -3,7 +3,9 @@ import React from "react";
 
 const Index = () => {
   return (
-    <div className="text-red-600 font-dancing text-center text-2xl kaybol">Eat Burger</div>
+    <div>
+      
+    </div>
   );
 };
 

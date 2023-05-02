4986d5486d66297b8c3a0eaf6c0f73cf528cfe9e
ireemtsn <79103069+ireemtsn@users.noreply.github.com>
2023-05-02 18:27:08 +0300

    react-icons

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -2,10 +2,10 @@ import Logo from "../ui/Logo";
 const Header = () => {
   return (
     <div className="h-[5.5rem] bg-secondary">
-      <div className="container mx-auto text-white flex justify-between ">
+      <div className="container mx-auto text-white flex justify-between items-center h-full ">
         <Logo />
-        <nav>
-          <ul className="flex gap-x-2">
+        <nav className="items-center ">
+          <ul className="flex gap-x-2 items-center h-full">
             <li className="px-[5px] py-[10px] uppercase hover:text-primary cursor-pointer">
               <a href="">Home</a>
             </li>
